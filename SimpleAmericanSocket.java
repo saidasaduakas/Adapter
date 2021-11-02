d9fc9b5c63ef8eeffd3aa9b063600b482d872387
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2021-11-02 17:10:09 +0600

    Create SimpleAmericanSocket.java

---
+++ SimpleAmericanSocket.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SimpleAmericanSocket implements AmericanSocket{
+
+    @Override
+    public void getPower() {
+        System.out.println("get 220 volt");
+    }
+}
